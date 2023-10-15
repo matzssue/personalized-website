00a899416325adbdcb80022dc7beca7bdef7dc98
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-15 18:40:54 +0200

    feat: add primary hover color

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,16 +1,19 @@
 export type Theme = {
   primary: string;
+  primaryHover: string;
 };
 
-export type ThemeNames = "dark" | "light";
+export type ThemeNames = 'dark' | 'light';
 
 export type Themes = Record<ThemeNames, Theme>;
 
 export const themes: Themes = {
   dark: {
-    primary: "#fd1d2078",
+    primary: '#fd1d2078',
+    primaryHover: '#fb2c2e',
   },
   light: {
-    primary: "#cde30878",
+    primary: '#cde30878',
+    primaryHover: '#ebff37',
   },
 };
