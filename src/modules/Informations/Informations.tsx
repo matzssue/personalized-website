cfccdfe76328b9312de011abcc14adb044da8fb8
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-15 18:34:05 +0200

    feat: change informations to be mapped from imported const

--- src/modules/Informations/Informations.tsx
+++ src/modules/Informations/Informations.tsx
@@ -1,70 +1,17 @@
-import styles from "./Informations.module.scss";
+import { starWarsInformations } from '../../constants/starWarsInformations';
 
-export const Informations = () => {
-  return (
-    <section id={styles.informations}>
-      <h2>Three things you should known about me </h2>
-      <ul>
-        <li>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
-            animi corrupti, dicta vero ullam adipisci fugiat suscipit quam.
-            Officia nihil enim repudiandae dolore possimus illo quidem eos
-            tempore unde impedit. Minima saepe deleniti mollitia libero beatae
-            dolores earum blanditiis ex dicta pariatur perferendis reprehenderit
-            tempora exercitationem optio, nemo soluta. Quaerat, labore ea ex
-            excepturi similique quasi quia porro at! Asperiores. Nesciunt cum
-            harum voluptatum deserunt inventore consequuntur rerum recusandae
-            ducimus perspiciatis ad! Quidem recusandae cumque aperiam saepe
-            iusto sequi neque, minima dolorum commodi iure. Culpa iusto
-            quibusdam quaerat quisquam blanditiis! Recusandae ab aut corporis
-            molestias fugit, voluptatem blanditiis cumque autem corrupti
-            eligendi cum animi iusto provident quos aliquam quis labore
-            laudantium ipsam sequi voluptatibus? Beatae aspernatur placeat
-            sequi? Soluta, possimus!
-          </span>
+import styles from './Informations.module.scss';
+
+export const Informations = () => (
+  <section id={styles.informations}>
+    <h2>Three things you should known about star wars </h2>
+    <ul>
+      {starWarsInformations.map(({ content, title }) => (
+        <li key={title}>
+          <p>{title}</p>
+          <span>{content}</span>
         </li>
-        <li>
-          <span>
-            {" "}
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
-            animi corrupti, dicta vero ullam adipisci fugiat suscipit quam.
-            Officia nihil enim repudiandae dolore possimus illo quidem eos
-            tempore unde impedit. Minima saepe deleniti mollitia libero beatae
-            dolores earum blanditiis ex dicta pariatur perferendis reprehenderit
-            tempora exercitationem optio, nemo soluta. Quaerat, labore ea ex
-            excepturi similique quasi quia porro at! Asperiores. Nesciunt cum
-            harum voluptatum deserunt inventore consequuntur rerum recusandae
-            ducimus perspiciatis ad! Quidem recusandae cumque aperiam saepe
-            iusto sequi neque, minima dolorum commodi iure. Culpa iusto
-            quibusdam quaerat quisquam blanditiis! Recusandae ab aut corporis
-            molestias fugit, voluptatem blanditiis cumque autem corrupti
-            eligendi cum animi iusto provident quos aliquam quis labore
-            laudantium ipsam sequi voluptatibus? Beatae aspernatur placeat
-            sequi? Soluta, possimus!
-          </span>
-        </li>
-        <li>
-          <span>
-            {" "}
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
-            animi corrupti, dicta vero ullam adipisci fugiat suscipit quam.
-            Officia nihil enim repudiandae dolore possimus illo quidem eos
-            tempore unde impedit. Minima saepe deleniti mollitia libero beatae
-            dolores earum blanditiis ex dicta pariatur perferendis reprehenderit
-            tempora exercitationem optio, nemo soluta. Quaerat, labore ea ex
-            excepturi similique quasi quia porro at! Asperiores. Nesciunt cum
-            harum voluptatum deserunt inventore consequuntur rerum recusandae
-            ducimus perspiciatis ad! Quidem recusandae cumque aperiam saepe
-            iusto sequi neque, minima dolorum commodi iure. Culpa iusto
-            quibusdam quaerat quisquam blanditiis! Recusandae ab aut corporis
-            molestias fugit, voluptatem blanditiis cumque autem corrupti
-            eligendi cum animi iusto provident quos aliquam quis labore
-            laudantium ipsam sequi voluptatibus? Beatae aspernatur placeat
-            sequi? Soluta, possimus!
-          </span>
-        </li>
-      </ul>
-    </section>
-  );
-};
+      ))}
+    </ul>
+  </section>
+);
