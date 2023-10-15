d21e8dc9dd19747f15b41cad5a64b4798890c952
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-15 18:30:54 +0200

    feat: add query client with query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import "./App.module.scss";
-import { Home } from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeContextProvider } from "./context/ThemeSelectorContext";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-function App() {
-  return (
+import { ThemeContextProvider } from './context/ThemeSelectorContext';
+import { AboutPage } from './pages/AboutPage';
+import { Home } from './pages/Home';
+import { Movie } from './pages/Movie';
+
+import './App.module.scss';
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache(),
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000,
+    },
+  },
+});
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    {process.env.NODE_ENV === 'development' && (
+      <ReactQueryDevtools initialIsOpen={false} position='top-right' />
+    )}
     <ThemeContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<Home />} path="/" />
+          <Route element={<Home />} path='/' />
+          <Route element={<AboutPage />} path='about' />
+          <Route path='movies'>
+            <Route element={<Movie />} path=':movieId' />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ThemeContextProvider>
-  );
-}
+  </QueryClientProvider>
+);
 
 export default App;
