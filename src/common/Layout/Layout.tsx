b9620b8c2b3aeb6dd30de7409221916761655795
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-15 18:32:28 +0200

    feat: add bb8 character image with button to hide it

--- src/common/Layout/Layout.tsx
+++ src/common/Layout/Layout.tsx
@@ -1,13 +1,20 @@
-import { ReactNode } from "react";
-import styles from "./Layout.module.scss";
-import { NavLinks } from "../NavLinks/NavLinks";
-import { navigationLinks } from "../../constants/navigationLinks";
+import { ReactNode } from 'react';
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
+
+import bb8Character from '../../assets/bb8.webp';
+import { navigationLinks } from '../../constants/navigationLinks';
+import { useThemeContext } from '../../context/ThemeSelectorContext';
+import { NavLinks } from '../NavLinks/NavLinks';
+
+import styles from './Layout.module.scss';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+  const { isImageHidden, toggleHideImage } = useThemeContext();
+
   return (
     <div className={styles.box}>
       <header>
-        <p>Mateusz Kluska - Portfolio</p>
+        <p className={styles.name}>Mateusz Kluska - Portfolio</p>
         <nav>
           <NavLinks navigationLinks={navigationLinks} />
         </nav>
@@ -16,6 +23,18 @@ export const Layout = ({ children }: { children: ReactNode }) => {
       <footer>
         <p>Mateusz Kluska 2023 @ All rights reserved</p>
       </footer>
+      <div className={`${styles['bb8-container']} ${isImageHidden ? styles.hidden : ''}`}>
+        <img
+          src={bb8Character}
+          className={`${styles['bb8-image']} ${isImageHidden ? styles.hidden : ''}`}
+        />
+      </div>
+      <button
+        className={`${styles['toggle-image-button']} ${isImageHidden ? styles.hidden : ''}`}
+        onClick={toggleHideImage}
+      >
+        {isImageHidden ? <BsFillArrowRightCircleFill /> : <BsFillArrowLeftCircleFill />}
+      </button>
     </div>
   );
 };
