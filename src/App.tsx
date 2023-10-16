c3a7cc80d09a0998508b60bf5e04630d010b4f4a
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-16 10:44:48 +0200

    feat: add error boundary, lazy loading, suspense

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-qu
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ThemeContextProvider } from './context/ThemeSelectorContext';
-import { AboutPage } from './pages/AboutPage';
-import { Home } from './pages/Home';
-import { Movie } from './pages/Movie';
+
+import Home from './pages/Home';
+
+import { Suspense, lazy } from 'react';
+import { Loader } from './common/Loader/Loader';
+import ErrorBoundary from './modules/ErrorBoundary/ErrorBoundary';
+import { Error404 } from './pages/Error404/Error404';
+
+const LazyAbout = lazy(() => import('./pages/AboutPage'));
+const LazyMovie = lazy(() => import('./pages/Movie'));
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache(),
@@ -21,17 +28,22 @@ const App = () => (
     {process.env.NODE_ENV === 'development' && (
       <ReactQueryDevtools initialIsOpen={false} position='top-right' />
     )}
-    <ThemeContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Home />} path='/' />
-          <Route element={<AboutPage />} path='about' />
-          <Route path='movies'>
-            <Route element={<Movie />} path=':movieId' />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeContextProvider>
+    <ErrorBoundary>
+      <ThemeContextProvider>
+        <BrowserRouter basename={'/personalized-website'}>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route element={<Error404 />} path='*' />
+              <Route element={<Home />} path='/' />
+              <Route element={<LazyAbout />} path='about' />
+              <Route path='movies'>
+                <Route element={<LazyMovie />} path=':movieId' />
+              </Route>
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </ThemeContextProvider>
+    </ErrorBoundary>
   </QueryClientProvider>
 );
 
