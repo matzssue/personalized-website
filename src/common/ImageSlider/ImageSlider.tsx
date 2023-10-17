73fed0e28eb6742083f89cb4f94db53b7b7c17aa
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-17 23:08:15 +0200

    feat: remove return function and length const

--- src/common/ImageSlider/ImageSlider.tsx
+++ src/common/ImageSlider/ImageSlider.tsx
@@ -7,19 +7,14 @@ import styles from './ImageSlider.module.scss';
 
 export const ImageSlider = ({ sliderData }: { sliderData: SpaceShip[] }) => {
   const [current, setCurrent] = useState(0);
-  const length = sliderData.length;
 
   const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent(current === sliderData.length - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent(current === 0 ? sliderData.length - 1 : current - 1);
   };
-
-  if (!Array.isArray(sliderData) || sliderData.length <= 0) {
-    return null;
-  }
 
   return (
     <div className={styles.slider}>
